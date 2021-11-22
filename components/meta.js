806841e5026c27eb2facf8f5ec693a9388e2ae04
Brian Parks <bparks@ignitesales.com>
2021-11-21 21:18:55 -0600

    meta fix

--- components/meta.js
+++ components/meta.js
@@ -15,10 +15,10 @@ export default function Meta({
       <meta name="author" content="Brian Parks" />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={image ? 'https://eatthisbeef.com/' : 'https://eatthisbeef.com/'} />
-      <meta property="og:url" content="http://eatthisbeef.com/" />
+      <meta property="og:url" content="https://eatthisbeef.com/" />
       <meta property="og:site_name" content="Eat This Beef!" />
       <meta property="og:description" content="DFW's most epic burger ranking publication of all time.  Feed your life-long burger quest in Dallas, TX." />
-      <link rel="shortcut icon" href="http://images.eatthisbeef.com/favicon.png" type="image/x-icon" />
+      <link rel="shortcut icon" href="https://images.eatthisbeef.com/favicon.png" type="image/x-icon" />
     </Head>
   )
 }
